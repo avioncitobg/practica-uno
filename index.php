2d07fad8f08d5fd68a01bd5144345a5677185302
avioncitobg <benjaminbg_1995@hotmail.com>
2026-08-11 12:36:33 -0500

    Se agrega un archivo index, donde tiene implementado una tabla en html:5

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>index</title>
+</head>
+
+<body>
+    <table>
+        <thead>
+            <tr>
+                <th>Header 1</th>
+                <th>Header 2</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td>Cell 1</td>
+                <td>Cell 2</td>
+            </tr>
+        </tbody>
+    </table>
+</body>
+
+</html>
